50af210bdf18acb0769ed8914580ff7bce924062
Yaroslav <yar21sav@gmail.com>
2022-12-03 22:49:46 +0300

    parser dockerfile fix, update compose

--- Parser/server.py
+++ Parser/server.py
@@ -1,21 +1,31 @@
+#! /usr/bin/env python3
+
 from fastapi import FastAPI
 import uvicorn
 import os
 from main import get_links, get_page_selenium, save_images
-
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("Parser")
 
 app = FastAPI(title="Avanpost hack - 'MISIS AI LAB' team")
 
 
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
 
 @app.get("/get_images")
 async def get_images(object, folder_name, galery_name):
+    save_dir = "/datasets/" + galery_name + "/" + folder_name
     try:
-        save_images(get_links(get_page_selenium(object)), folder_name=folder_name, galery_name=galery_name)
-    except:
+        logger.info("Processing request for \"{}\", saving to \"{}\"".format(object, save_dir))
+        save_images(get_links(get_page_selenium(object)), save_dir)
+    except Exception as e:
+        logger.error(e)
         return {'status': 'failed'}
     return {'status': '200'}
 
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8005)
