7e8cb54c1a1be7df9ba40afd01be9e6f318b4180
komtriangle <komlevdanila742@gmail.com>
2022-12-04 08:53:36 +0300

    api

--- API/AvanPost.API/Controllers/TrainsController.cs
+++ API/AvanPost.API/Controllers/TrainsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
 using System;
+using System.IO;
 
 namespace AvanPost.API.Controllers
 {
@@ -19,17 +20,32 @@ namespace AvanPost.API.Controllers
         private readonly AvanpostContext _context;
         private readonly RabbitMQSendTrain _rabbitMqSendTrain;
         private const string DATASET_PATH = "default";
+        private readonly AppSettings _appSettings;
 
-        public TrainsController(AvanpostContext context, IOptions<AppSettings> options,  IOptions<RabbitMQConfig> rabbitMqConf)
+        public TrainsController(AvanpostContext context, IOptions<AppSettings> options,  IOptions<RabbitMQConfig> rabbitMqConf, IOptions<AppSettings> settings)
         {
             _rabbitMqSendTrain = new RabbitMQSendTrain(rabbitMqConf.Value);
             _context = context;
+            _appSettings = settings.Value;
         
         }
 
         [HttpPost("Train")]
         public async Task Train(TrainRequest request)
         {
+
+            foreach(var @class in request.Classes)
+            {
+                var _dbClass = await _context.DataClasses.FindAsync(@class);
+
+                if(_dbClass != null)
+                {
+                    foreach (string newPath in Directory.GetFiles(Path.Combine(_appSettings.ImagesFolder, _dbClass.Name)))
+                    {
+                        System.IO.File.Copy(newPath, newPath.Replace(_appSettings.ImagesFolder, _appSettings.ToTrainImages), true);
+                    }
+                }
+            }
             var key = Guid.NewGuid().ToString();
 
             await _context.Trains.AddAsync(new Avanpost.Data.Entities.Train()
